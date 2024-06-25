91af30bee4c076b626e1389a29d839dff78fc84d
Luis Martinez <luis.martinez@venafi.com>
2024-06-25 13:43:56 -0600

    functional xor with -hex optiobn

--- cmd/xor.go
+++ cmd/xor.go
@@ -1,14 +1,54 @@
 package cmd
 
 import (
+	"encoding/hex"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var xorCmd = &cobra.Command{
+var isHex bool
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&isHex, "hex", false, "specify if entry value is hex")
+}
+
+var rootCmd = &cobra.Command{
 	Use: "",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hello cobra")
+		var buf1 []byte
+		var buf2 []byte
+		var err error
+
+		if isHex {
+			buf1, err = hex.DecodeString(args[0])
+			if err != nil {
+				log.Fatal("failed to decode hex1: " + err.Error())
+			}
+
+			buf2, err = hex.DecodeString(args[1])
+			if err != nil {
+				log.Fatal("failed to decode hex2: " + err.Error())
+			}
+		} else {
+			buf1 = []byte(args[0])
+			buf2 = []byte(args[1])
+		}
+
+		out := make([]uint8, len(buf1))
+		for i := range buf1 {
+			out[i] = buf1[i] ^ buf2[i%len(buf2)]
+		}
+
+		fmt.Printf("%x\n", out)
 	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
-	"log"
-	"os"
+	"github.com/JarlMartinez/xor/cmd"
 )
 
 func main() {
-
-	hex1 := os.Args[1]
-	hex2 := os.Args[2]
-
-	buf1, err := hex.DecodeString(hex1)
-	if err != nil {
-		log.Fatal("failed to decode hex1: " + err.Error())
-	}
-
-	buf2, err := hex.DecodeString(hex2)
-	if err != nil {
-		log.Fatal("failed to decode hex2: " + err.Error())
-	}
-
-	out := make([]uint8, len(buf1))
-	for i := range buf1 {
-		out[i] = buf1[i] ^ buf2[i]
-	}
-
-	fmt.Printf("%x\n", out)
+	cmd.Execute()
 }
